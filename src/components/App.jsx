3373f7b0e3abd2150a877b43e544fa91cc9e4d99
Valerii Pozhydaiev <p_vf@i.ua>
2023-05-02 17:34:35 +0300

    init project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,9 +7,17 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
+      {/* <Searchbar /> */}
+      {/* <ImageGallery /> */}
+      {/* <ImageGalleryItem /> */}
+      {/* <Loader /> */}
+      {/* <Button /> */}
+      {/* <Modal></Modal> */}
+      {/* https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12 */}
+      {/* const API_KEY='35178730-35412664985809edebeff8fc3' */}
       React homework template
     </div>
   );
