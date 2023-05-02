e7482d009492a4c07ae262813b52efa34c7f2e67
Valerii Pozhydaiev <p_vf@i.ua>
2023-05-02 18:11:05 +0300

    sdd structure

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,21 @@
+const Searchbar = () => {
+  return (
+    <header class="searchbar">
+      <form class="form">
+        <button type="submit" class="button">
+          <span class="button-label">Search</span>
+        </button>
+
+        <input
+          class="input"
+          type="text"
+          autocomplete="off"
+          autofocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
+
+export default Searchbar;
